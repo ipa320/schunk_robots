a1b943f1678c6ac42794d9204c5ad46792a8fd0a
Thiago de Freitas <thiagodefreitas@gmail.com>
2013-04-16 19:45:46 +0200

    Temporary commit

---
+++ schunk_arm_navigation/demo_arm.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+
+#################################################################
+##\file
+#
+# \note
+# Copyright (c) 2013 \n
+# Fraunhofer Institute for Manufacturing Engineering
+# and Automation (IPA) \n\n
+#
+#################################################################
+#
+# \note
+# Project name: Care-O-bot Research
+# \note
+# ROS package name: 
+#
+# \author
+# Author: Thiago de Freitas Oliveira Araujo, 
+# email:thiago.de.freitas.oliveira.araujo@ipa.fhg.de
+# \author
+# Supervised by: Florian Weisshardt, email:florian.weisshardt@ipa.fhg.de
+#
+# \date Date of creation: April 2013
+#
+# \brief
+# Powerball arm class
+#
+#################################################################
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# - Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer. \n
+# - Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution. \n
+# - Neither the name of the Fraunhofer Institute for Manufacturing
+# Engineering and Automation (IPA) nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission. \n
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License LGPL as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License LGPL for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License LGPL along with this program.
+# If not, see < http://www.gnu.org/licenses/>.
+#
+#################################################################
+import roslib; 
+roslib.load_manifest('schunk_arm_navigation')
+import rospy
+import rosparam
+import actionlib
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+from control_msgs.msg import *
+roslib.load_manifest('cob_script_server')
+from simple_script_server import *
+
+class powerball_arm:
+    
+    def __init__(self):
+       
+        self._client = actionlib.SimpleActionClient('arm_controller/follow_joint_trajectory', FollowJointTrajectoryAction)
+        self._client.wait_for_server()
+        self.available_poses = ['home', 'folded', 'wave_left', 'wave_right']
+        self.sss = simple_script_server()
+
+    def move_arm(self):
+    
+        for pose in self.available_poses:
+    
+            self.sss.move('arm', pose)
+            rospy.sleep(10)
+    
+if __name__=="__main__":
+
+    rospy.init_node("powerball_arm")
+    pball = powerball_arm()
+    r = rospy.Rate(10)
+    
+    while not rospy.is_shutdown():
+
+        pball.move_arm()
+        r.sleep()
